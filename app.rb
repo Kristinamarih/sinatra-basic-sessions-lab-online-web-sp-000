b9ddc509b184a01d0af5265f62e404cb733327c9
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 13:35:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+  enable :sessions
+  set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/checkout' do 
+    session[:item] = session 
+    @item = params[:item]
+  end
+  
+    
 end
